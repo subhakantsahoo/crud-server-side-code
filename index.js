354040557c343ccc939c328a93bcf6cd07175d45
LAP063 <subhakanta.sahoo>
2026-04-09 10:57:57 +0530

    fixed

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 
 const app = require('express')();
 const port = process.env.PORT || 3000;
- const passport=require('passport');
-const bodyParser=require('body-parser');
-const{connectMongo}=require("./config/connect");
-const routes=require('./routes/index');
-const cors=require("cors");
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const { connectMongo } = require("./config/connect");
+const routes = require('./routes/index');
+const cors = require("cors");
 const { collection, insertMany } = require('./models/movies-schema');
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 connectMongo();
 app.use(bodyParser.json());
 app.use(cors())
-app.get('/1', (req, res) => { 
-    res.send('this 2nd page');
-  })
-
+app.get('/1', (req, res) => {
+  res.send('this 2nd page');
+})
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', message: 'Server is running' });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
 
-app.use('/api',routes);
-  
+app.use('/api', routes);
+
 
 
